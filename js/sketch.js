44f932bf4a2502b3353b0af314394025f82662c3
DrHrendalf <gatyybujd@yandex.ru>
2015-12-02 00:44:26 +0300

    final

--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,48 @@
 $(document).ready(function(){
-	start();
+	var n = 20;
+
+	for(var i = 1;i<n*n+1;i++){
+		$('#container').append('<div class="cell"></div>');
+	}
+	
 	$(document).on('mouseenter','div div.cell', function(){
-		$(this).css("background-color", getRandomColor());
+		var currColor = $(this).css("background-color").substr(3);
+		
+		var colors = currColor.replace('(','').replace(')','').split(', ');
+		
+		var currRed = colors[0];
+		var currGreen = colors[1];
+		var currBlue = colors[2];
+
+		if(currColor==="(255, 255, 255)"){
+			$(this).css("background-color", getRandomColor());
+		}else{
+				currRed=Math.abs(currRed) - Math.abs(Math.floor(currRed/2));
+
+				currGreen=Math.abs(currGreen) - Math.abs(Math.floor(currGreen/2));
+
+				currBlue=Math.abs(currBlue) - Math.abs(Math.floor(currBlue/2));
+			
+			$(this).css("background-color", "rgb("+currRed.toString()+', '+
+				currGreen.toString()+', '+
+				currBlue.toString()+')');
+		}
+		//$(this).fadeTo(0,0);
+		//$(this).fadeTo(200,1);
 	});
 });
 
 
 function start(){
 	$('div div.cell').remove();
-	var n = $('input#n').val();
-	$('#container').width(n*50 + "px");
-	$('#container').height(n*50 + "px");
+	var n = prompt('enter new grid size');
+
 	for(var i = 1;i<n*n+1;i++){
 		$('#container').append('<div class="cell"></div>');
 	}
+
+	$('.cell').width(Math.floor(600/n) + "px");
+	$('.cell').height(Math.floor(600/n) + "px");
 }
 
 function getRandomColor() {
